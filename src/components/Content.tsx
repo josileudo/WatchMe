22d6bc27ddf06b6757c0ee4c10a03bfe1418ac9c
Josileudo Rodrigues <josileudorodrigues75@gmail.com>
2021-05-03 20:40:54 -0300

    concluded content

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,22 +1,49 @@
-import {useState} from "react"
+import { MovieCard } from "../components/MovieCard"
 
 import "../styles/content.scss"
 
+interface Genre {
+  id: number;
+  name: string;
+  title: string;
+}
 
-export function Content() {
+interface Movie {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+interface MovieProps {
+  movies: Array <Movie>
+  selectedGenre: Genre;
+}
+
+export function Content({movies, selectedGenre} : MovieProps) {
   return (
     <div className="container">
-        <header>
-          <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
-        </header>
+      <header>
+        <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
+      </header>
 
-        <main>
-          <div className="movies-list">
-            {movies.map(movie => (
-              <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
-            ))}
-          </div>
-        </main>
-      </div>
+      <main>
+        <div className="movies-list">
+          {movies.map(movie => (
+            <MovieCard
+              key ={movie.imdbID} 
+              title={movie.Title} 
+              poster={movie.Poster} 
+              runtime={movie.Runtime} 
+              rating={movie.Ratings[0].Value}
+            />
+          ))}
+        </div>
+      </main>
+    </div>
   )
 }
